740d88ad5743fb6a7b3608d6093cdcf4c9eba6ff
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-08 23:14:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,14 @@
 # code your #valid_move? method here
-def valied_move?
+def valid_move?(board, index)
   
   
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    taken = false 
+  else
+    taken = true 
+  end
+end
